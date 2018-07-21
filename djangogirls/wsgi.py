627a19e8cc51573ee7d28edc122e9180f9119932
jjolbo <jeongha3751@gmail.com>
2018-07-21 14:41:21 +0900

    modified wsgi.py

--- djangogirls/wsgi.py
+++ djangogirls/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangogirls.settings")
+path = '/home/jeongha/my-first-blog'
 
-application = get_wsgi_application()
+if path not in sys.path:
+    sys.path.append(path)
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+application = StaticFilesHandler(get_wsgi_application())
